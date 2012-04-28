129ae102e34943cb3fa4419a366fa91bf738883a
Luiz Andre <luis.ta@gmail.com>
2012-04-28 09:41:45 -0300

    inicio

---
+++ src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -0,0 +1,22 @@
+package br.com.caelum.tarefas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.caelum.tarefas.dao.TarefaDAO;
+import br.com.caelum.tarefas.modelo.Tarefa;
+
+@Controller
+public class TarefasController {
+	@RequestMapping("novaTarefa")
+	public String form() {
+		return "tarefa/formulario";
+	}
+	
+	@RequestMapping("adicionaTarefa")
+	public String adiciona(Tarefa tarefa) {
+		TarefaDAO dao = new TarefaDAO();
+		dao.adiciona(tarefa);
+		return "tarefa/adicionada";
+	}
+}
